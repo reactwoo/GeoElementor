3212c03e9079160f935cd43837f5135e726d0c9f
reactwoo <info@reactwoo.com>
2025-09-08 16:20:21 +0100

    issue popup settings

--- elementor-geo-popup.php
+++ elementor-geo-popup.php
@@ -23,7 +23,7 @@ if (!defined('ABSPATH')) {
 }
 
 // Define plugin constants
-define('EGP_VERSION', '1.0.0');
+define('EGP_VERSION', '1.0.1');
 define('EGP_PLUGIN_FILE', __FILE__);
 define('EGP_PLUGIN_DIR', plugin_dir_path(__FILE__));
 define('EGP_PLUGIN_URL', plugin_dir_url(__FILE__));
